a0fb7ef15169e41a47b290227703cad9e8b5e13b
Miro Spönemann <miro.spoenemann@typefox.io>
2016-07-27 13:31:19 +0200

    Created language server example with Xtext

---
+++ io.typefox.vscode.ide/src/main/java/io/typefox/vscode/ide/MyLanguageIdeSetup.xtend
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2016 TypeFox GmbH (http://www.typefox.io) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package io.typefox.vscode.ide
+
+import com.google.inject.Guice
+import io.typefox.vscode.MyLanguageRuntimeModule
+import io.typefox.vscode.MyLanguageStandaloneSetup
+
+/**
+ * Initialization support for running Xtext languages without Equinox extension registry.
+ */
+class MyLanguageIdeSetup extends MyLanguageStandaloneSetup {
+
+	override createInjector() {
+		Guice.createInjector(new MyLanguageRuntimeModule, new MyLanguageIdeModule)
+	}
+}
---
+++ io.typefox.vscode.ide/vscode-extension/src/extension.ts
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2016 TypeFox GmbH (http://www.typefox.io) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+'use strict';
+
+import * as net from 'net';
+import * as path from 'path';
+
+import { workspace, Disposable, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+	// The server is a locally installed Java application
+	let executable = process.platform == 'win32' ? 'xtext-server-example.bat' : 'xtext-server-example';
+    let serverLauncher = context.asAbsolutePath(path.join('xtext-server-example', 'bin', executable));
+    
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        run : { command: serverLauncher },
+        debug: { command: serverLauncher, args: ['-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044'] }
+    }
+    
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: ['mydsl'],
+        synchronize: {
+            // Synchronize the setting section 'languageServerExample' to the server
+            configurationSection: 'languageServerExample',
+            // Notify the server about file changes to '.clientrc files contain in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    }
+	
+	// Create the language client and start the client.
+	let disposable = new LanguageClient('Xtext Server', serverOptions, clientOptions).start();
+	
+	// Push the disposable to the context's subscriptions so that the 
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(disposable);
+}
